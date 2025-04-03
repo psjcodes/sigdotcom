cc8e71f17518c4a02635b010dafc4af60602459a
Pranay Jha <pranay.jha@gmail.com>
2025-04-03 13:41:49 -0700

    app -> interest

--- app/join/components/Hero.tsx
+++ app/join/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <Stack align="center">
       <Title mb={20} c="green">Interested in joining SIG?</Title>
       <Button component="a" href="https://forms.gle/TJWsNZbAJNq8x6jP7" target="_blank" variant="gradient"
-      gradient={{ from: 'green.3', to: 'green.9', deg: 90 }} style={{ boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)' }}>Application Form</Button>
+      gradient={{ from: 'green.3', to: 'green.9', deg: 90 }} style={{ boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)' }}>Interest Form</Button>
     </Stack>
   )
 }
